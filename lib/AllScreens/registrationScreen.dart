fd1833ffef414401c9277e30605fb91b424a5b4d
“Adebayo <Adebayo.osunbiyi.a@gmail.com>
2022-05-18 06:33:06 +0100

    Modification of FlutterToastMessage

--- lib/AllScreens/registrationScreen.dart
+++ lib/AllScreens/registrationScreen.dart
@@ -216,7 +216,14 @@ class RegistrationScreen extends StatelessWidget {
   }
 
   void displayToastMessage(String msg, BuildContext context) {
-    Fluttertoast.showToast(msg: msg);
+    Fluttertoast.showToast(
+        msg: msg,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.yellow,
+        fontSize: 16.0);
   }
 }
 
